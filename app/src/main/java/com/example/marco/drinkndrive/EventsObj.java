92e4a1e50e58db2c748462b61ddf329a7d91c247
Argtes <stfu.gtfo@laposte.net>
2016-12-16 17:03:34 +0100

    the fuck
    oui ?

---
+++ app/src/main/java/com/example/marco/drinkndrive/EventsObj.java
@@ -0,0 +1,32 @@
+package com.example.marco.drinkndrive;
+
+/**
+ * Created by Marco on 15/12/2016.
+ */
+
+public class EventsObj {
+    private long id;
+
+    public String getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(String drinks) {
+        this.drinks = drinks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String drinks;
+
+
+    public String toString() {
+        return drinks;
+    }
+}
